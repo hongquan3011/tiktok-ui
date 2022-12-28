5f1b615d11257f2dea4393e63eb04f7953c6fc1a
hongquan3011 <hongquan0165@gmail.com>
2022-12-28 11:32:38 +0700

    Sidebar 2

--- src/layouts/components/Sidebar/Sidebar.js
+++ src/layouts/components/Sidebar/Sidebar.js
@@ -3,6 +3,7 @@ import Menu, { MenuItem } from './Menu';
 import styles from './Sidebar.module.scss';
 import config from '~/config';
 import { HomeIcon,HomeActiveIcon, UserGroupIcon,UserGroupActiveIcon, LiveIcon,LiveActiveIcon} from '~/components/Icons';
+import SuggestedAccounts from '~/components/SuggestedAccounts';
 const cx = classNames.bind(styles);
 function Sidebar() {
     return (
@@ -11,8 +12,11 @@ function Sidebar() {
                 <MenuItem title="For Your" to={config.routers.home} icon={<HomeIcon />} activeIcon={<HomeActiveIcon/>} />
                 <MenuItem title="Following" to={config.routers.following} icon={<UserGroupIcon />} activeIcon={<UserGroupActiveIcon/>} />
                 <MenuItem title="LIVE" to={config.routers.live} icon={<LiveIcon />} activeIcon={<LiveActiveIcon/>}/>
-            </Menu>
+            </Menu> 
+            <SuggestedAccounts label='Suggested Accounts'/>
+            <SuggestedAccounts label='Following Accounts'/>
         </aside>
+       
     );
 }
 
